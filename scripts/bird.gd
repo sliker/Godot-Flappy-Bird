101425524a2661e57a1811e164b734967b1f2107
David <zero.blend@gmail.com>
2019-09-18 22:40:33 -0500

    first commit

---
+++ scripts/bird.gd
@@ -0,0 +1,23 @@
+# script: bird
+
+extends RigidBody2D
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	if rad2deg(rotation) < -30:
+		rotation = deg2rad(-30)
+		angular_velocity = 0
+
+	if linear_velocity.y > 0:
+		angular_velocity = 1.5
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_SPACE:
+			flap()
+			
+func flap():
+	angular_velocity = -3
+	linear_velocity = Vector2(linear_velocity.x, -150)
